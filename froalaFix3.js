a4f0018ed14223d7155157c97ec9d9ed1378b114
ttt ttt <anastasya.doval12@gmail.com>
2024-05-07 00:25:43 +0300

    убрал лишнюю функцию

--- froalaFix3.js
+++ froalaFix3.js
@@ -2,23 +2,14 @@
 FroalaEditor.PLUGINS.insertPopupFix = function(editor) { 
     let isFullScreen;
 
-    function getElementSize(element) {
-        const width = element.clientWidth;
-        const height = element.clientHeight;
-        return {width: width, height: height};
-    }
-
     function fixPopupPosListener(editor) {
         if (isFullScreen === true) return;
 
         let insertLinkButton = $(editor.$tb.find('.fr-command[data-cmd="insertLink"]'))[0];
         let insertLinkPopup = editor.popups.get("link.insert")[0];
-
-        let linkButtonSize = getElementSize(insertLinkButton);
-        let popupSize = getElementSize(insertLinkPopup);
-
+        
         let popupOffsetY = insertLinkPopup.offsetTop;
-        let newPopupOfssetY = ( popupOffsetY - (linkButtonSize.height + popupSize.height + 10));
+        let newPopupOfssetY = ( popupOffsetY - (insertLinkButton.clientHeight + insertLinkPopup.clientHeight + 10));
         
         insertLinkPopup.style.top = newPopupOfssetY + "px";
     }
